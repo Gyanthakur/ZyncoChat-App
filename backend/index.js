b2e7402c5a1cb0a7e8c832a1304b2c6b7db5b896
Gyanthakur <gps.96169@gmail.com>
2024-12-29 13:39:09 +0530

    update: index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connectDB = require("./db.js");
 const cors = require("cors");
 const http = require("http");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const { initSocket } = require("./socket/index.js");
 const app = express();
 app.use(cors());
@@ -11,7 +11,7 @@ app.use(express.json({ limit: "50mb" }));
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Zynco Chat App is working fine!");
 });
 app.use("/auth", require("./Routes/auth_routes.js"));
 app.use("/user", require("./Routes/userRoutes.js"));
